c13d192ba5fcda6dccec7791778966205fd67a68
cleverRondrigues <118025019+cleverRondrigues@users.noreply.github.com>
2025-02-19 18:35:11 -0300

    Update automaçao_de_mensagens.py

--- automaçao_de_mensagens.py
+++ automaçao_de_mensagens.py
@@ -3,10 +3,10 @@ import pandas as pd
 #armazena as informaços das pessoas
 dados = {'nome':[],'idade':[],'numero':[]}
 
-while True:
+while True: #inicia o lope perguntado nome é idade é numero de telefone
     nome = str(input('nome\n=>'))
     idade = int(input('idade\n=>'))
-    numero = input('numero de telefone\n=>')
+    numero = input('numero de telefone\n=>')#eu quer ver se consigo fazer que o codigo envie uma mensgem para todos os numeros salvos
     
     dados['nome'].append(nome)
     dados['idade'].append(idade)
@@ -18,5 +18,5 @@ while True:
     if continuar.lower()== 'n':
        planilha= input('salvar as informaços em uma planilha s/n\n=>')
        if planilha.lower()== 's':
-           df.to_excel('meu_arquivo.xlsx', index=False)
+           df.to_excel('meu_arquivo.xlsx', index=False) #cria uma planilha com todas as informaços das pessas
            break
